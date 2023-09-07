03c732cf8af4e0e65f322a4bedacddda581d2fe0
giuprofilo <giulliavoloton@gmail.com>
2023-09-06 22:07:40 -0300

    att app.use

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import connectToDB from "./config/db.config.js";
 
 import userRouter from "./routes/user.routes.js";
 import uploadRoute from "./routes/upload.routes.js";
+import schoolRouter from "./routes/school.routes.js";
+import notfRouter from "./routes/notification.routes.js";
+import subjectRouter from "./routes/subject.routes.js";
 
 dotenv.config();
 
@@ -15,14 +18,20 @@ app.use(express.json()); // configuração do servidor para aceitar e receber ar
 
 app.use("/user", userRouter);
 
+app.use("/school", schoolRouter);
+
 app.use("/upload", uploadRoute);
 
+app.use("/notification", notfRouter);
+
+app.use("/subject", subjectRouter);
+
 connectToDB()
-   .then(
-      app.listen(process.env.PORT, () => {
-         console.log(`Server up and running at port ${process.env.PORT}`);
-      })
-   )
-   .catch((error) => {
-      console.log("Error connecting to database: ", error);
-   });
+  .then(
+    app.listen(process.env.PORT, () => {
+      console.log(`Server up and running at port ${process.env.PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log("Error connecting to database: ", error);
+  });
