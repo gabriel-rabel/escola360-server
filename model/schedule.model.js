6bde2c50e88ae8a725df129425f9e14d635a0b65
giuprofilo <giulliavoloton@gmail.com>
2023-09-15 05:23:36 -0300

    rotas e model schedule ok

---
+++ model/schedule.model.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const scheduleSchema = new Schema(
+  {
+    user: [{ type: Schema.Types.ObjectId, required: true, ref: "User" }],
+    subjects: [{ type: Schema.Types.ObjectId, required: true, ref: "Subject" }],
+    status: {
+      type: String,
+      enum: ["ABERTA", "FECHADA", "CANCELADA"],
+      default: "ABERTA",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Schedule", scheduleSchema);
